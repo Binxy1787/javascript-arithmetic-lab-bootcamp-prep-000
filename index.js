bf455ba32c4c59f6a036d5d55f26ff00de18b9d4
Jameson Nelson <nelson_jameson@yahoo.com>
2019-01-24 22:30:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function add(a, b){
   
 }
+
+function subtract(a, b){
+  
+}
+
+function multiply(a, b){
+  
+}
+
+function divide(a, b){
+  
+}
+
+function inc(a, b){
+  
+}
+
+function dec(a, b){
+  
+}
+
+function makeInt(a, b){
+  
+}
